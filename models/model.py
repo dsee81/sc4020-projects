5448facf4d6a9e8d1ffe52428fdd05cc3bab6f33
sahanarahim <sahanarahim2804@gmail.com>
2025-09-20 18:48:15 +0800

    gcn models added

---
+++ models/model.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+import torch
+from torch.nn import Linear
+from torch_geometric.nn import GCNConv
+import torch
+from torch_geometric.datasets import Planetoid
+from torch_geometric.utils import to_networkx
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch.nn as nn
+import networkx as nx
+from torch_geometric.utils import degree
+from collections import Counter
+import torch_geometric.transforms as T
+from torch_geometric.utils import to_dense_adj
+from layer import *
+
+class GCNModel(torch.nn.Module): #one which takes in the number of layers
+    def __init__(self, input_dim, number_of_classes, layers = 3, hidden_layer=16, layer_name='GCNLayer', dp_rate=0):
+        """GNNModel.
+
+        Args:
+            input_dim: Dimension of input features
+            hidden_layer: Dimension of hidden features
+            number_of_classes: Dimension of the output features. Usually number of classes in classification
+            layers: Number of "hidden" graph layers
+            layer_name: String of the graph layer to use
+            dp_rate: Dropout rate to apply throughout the network
+            kwargs: Additional arguments for the graph layer (e.g. number of heads for GAT)
+
+        """
+        super(GCNModel, self).__init__()
+        
+        self.gcn_layers = {'GCNLayer': GCNLayer, 'GCN': GCNConv}
+        gcn_layer = self.gcn_layers[layer_name]
+
+        model_layers = [gcn_layer(input_dim, hidden_layer)]
+        model_layers.append(nn.ReLU())
+        model_layers.append(nn.Dropout(dp_rate))
+
+        for i in range(layers):
+          model_layers.append(gcn_layer(hidden_layer, hidden_layer))
+          model_layers.append(nn.ReLU())
+          model_layers.append(nn.Dropout(dp_rate))
+
+        model_layers.append(gcn_layer(hidden_layer, number_of_classes))
+        self.model_layers = nn.Sequential(*model_layers)
+
+    def forward(self, x, A):
+      for conv in self.model_layers:
+        if isinstance(conv, [GCNLayer, GCNConv]):
+          x = conv(x, A)
+        else:
+          x = conv(x)
+
+      output = torch.softmax(x, dim=1)
+
+      return output
+
